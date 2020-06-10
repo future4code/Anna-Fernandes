df33a9debd8fa89e953b7aa895b1d957e842427b
Anna Fernandes <anna.cbf@gmail.com>
2020-06-10 20:29:50 -0300

    Criei os posts em nova url, mas de outra maneira, usando sessionStorage

--- semana4/objetos/index.js
+++ semana4/objetos/index.js
@@ -23,18 +23,43 @@ let publicarPost = (post) => {
         autor: author.value,
         imagem: image.value,
         texto: text.value
-    } 
-   
+    }
+
     todosPosts.push(post);
+    sessionStorage.setItem("todos", todosPosts);
+    sessionStorage.setItem("titulo", post.titulo);
+    sessionStorage.setItem("autor", post.autor);
+    sessionStorage.setItem("imagem", post.imagem);
+    sessionStorage.setItem("texto", post.texto);
 
     if ((post.titulo === "")|| (post.autor === "") || (post.texto === "")) {
         alert("Os campos de título, autor e texto devem estar preenchidos!");
     } else {
-        window.location = "post.html?post.titulo="+ post.titulo + "&post.autor=" + post.autor + "&post.imagem=" + post.imagem + "&post.texto=" + post.texto;
+        window.location = "post.html"
     }
 
     title.value = "";
     author.value = "";
     image.value = "";
     text.value = "";
+}
+
+const postTitulo = sessionStorage.getItem("titulo");
+const postAutor = sessionStorage.getItem("autor");
+const postImagem = sessionStorage.getItem("imagem");
+const postTexto = sessionStorage.getItem("texto");
+
+if(postPage) {
+    postsContainer.innerHTML += `
+    <div class="post">
+    <h2>${postTitulo}</h2>
+    <p class="post-author">por ${postAutor}</p>
+    <img class="post-image" src=${postImagem}>
+    <p class="paragraph">${postTexto}</p>
+    </div>
+    `
+}
+
+let createPost = () => {
+    window.location = "index.html"
 }
