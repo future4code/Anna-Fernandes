e874766432b33a37ed4e686e0121ea22bcdd21b8
Anna Fernandes <anna.cbf@gmail.com>
2020-06-10 18:56:53 -0300

    Refiz com o post criado em uma nova página. Foi sofrido.

--- semana4/objetos/index.js
+++ semana4/objetos/index.js
@@ -9,58 +9,32 @@ const postPage = document.getElementById("post-page");
 let post;
 let todosPosts = [];
 
-let mostrarCreatePage = () => {
-    createPage.classList.remove("display-none")
-    postPage.classList.add("display-none")
-}
-
 let pressionaEnter = () => {
     if (event.key === 'Enter') {
         publicarPost();
     }
 }
 
-let publicarPost = () => {
+let publicarPost = (post) => {
+    event.preventDefault()
 
     post = {
         titulo: title.value,
         autor: author.value,
         imagem: image.value,
         texto: text.value
-    }    
+    } 
+   
+    todosPosts.push(post);
 
-    
     if ((post.titulo === "")|| (post.autor === "") || (post.texto === "")) {
         alert("Os campos de título, autor e texto devem estar preenchidos!");
     } else {
-        postPage.classList.remove("display-none");
-
-        postsContainer.innerHTML += `
-        <div class="post">
-            <h2>${post.titulo}</h2>
-            <p class="post-author">por ${post.autor}</p>
-            <img class="post-image" src=${post.imagem}>
-            <p class="paragraph">${post.texto}</p>
-        </div>
-        `
-        createPage.classList.add("display-none");
+        window.location = "post.html?post.titulo="+ post.titulo + "&post.autor=" + post.autor + "&post.imagem=" + post.imagem + "&post.texto=" + post.texto;
     }
-
-    todosPosts.push(post);
 
     title.value = "";
     author.value = "";
     image.value = "";
     text.value = "";
-
-}
-
-let mostrarTodosPosts = () => {
-    console.log(post)
-    if (post === undefined){
-        alert("Não há posts. Comece criando um.")
-    } else {
-        createPage.classList.add("display-none")
-        postPage.classList.remove("display-none")
-    }
 }
