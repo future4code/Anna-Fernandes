0870fc3d24502ddb0a1b4456271a24cbfa1728c3
Anna Fernandes <anna.cbf@gmail.com>
2020-06-12 10:47:25 -0300

    Add pasta lista-de-exercícios com os exercícios de interpretação de código

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,11 @@
+// 1. Exercícios de interpretação de código
+
+// RESPOSTAS
+    // 1. R$502,13 (dólar a R$5,0213)
+    // 2. 165
+    //    udefinied. alert: Tipo de investimento informado incorreto!
+    // 3. Quantidade total de números14
+    //    6
+    //    8
+    //  4.-10
+    //    1590 
