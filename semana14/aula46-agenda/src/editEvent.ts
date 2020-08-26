7f26ad37dc2f33e32e30ad5514f2c4b8667a0fa5
Anna Fernandes <anna.cbf@gmail.com>
2020-08-26 16:38:22 -0300

    Add função editar

--- semana14/aula46-agenda/src/editEvent.ts
+++ semana14/aula46-agenda/src/editEvent.ts
@@ -2,6 +2,12 @@ import fs from "fs";
 import moment from "moment";
 moment.locale('pt-br');
 
+type event = {
+    name: string,
+    description: string,
+    startAt: moment.Moment,
+    finishAt: moment.Moment
+}
 
 const fileData: string = fs.readFileSync('./events.json').toString();
 
@@ -10,12 +16,20 @@ let events: any = JSON.parse(fileData);
 const name: string = process.argv[2]
 const date: moment.Moment = moment(process.argv[3], "DD/MM/YYYY HH:mm")
 
-events = events.filter( (event: any) => {
+events = events.map( (event: any) => {
     if(event.name.includes(name)) {
+        const newEvent: any = {
+            ...event,
+            finishAt: date
+        } 
+        return newEvent
+    } else {
         return event
     }
 })
 
 const eventsAsString: string = JSON.stringify(events, null, 3);
 
-console.log(eventsAsString)
+fs.writeFileSync('./events.json', eventsAsString)
+
+console.log("Evento alterado com sucesso!")
