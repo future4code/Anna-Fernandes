42f91e0583361e328c90b8e9356446e7a8201360
Anna Fernandes <anna.cbf@gmail.com>
2020-10-02 18:56:18 -0300

    Add endpoint de pegar profile no front

--- semana19/projeto/frontend/src/pages/ProfilePage/ProfilePage.js
+++ semana19/projeto/frontend/src/pages/ProfilePage/ProfilePage.js
@@ -1,14 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useHistory } from 'react-router-dom';
 
 import { Container } from '@material-ui/core';
 import { CenterObjects, MainContainer } from '../../styles/mainStyles';
 
 const ProfilePage = () => {
+    const [ profile, SetProfile] = useState([]);
+    const history = useHistory();
+
+    const getEventInfo = async() => {
+      try {
+        const axiosConfig = {
+            headers: {
+                Authorization: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjFjMjlkMjU4LWQxYzktNDBhZC1hNmE0LTA5ZjZiMjY1OWI5NSIsInJvbGUiOiJBRE1JTiIsImlhdCI6MTYwMTY3NTUyNCwiZXhwIjoxNjAxNjc1ODI0fQ.3S9LLa1SXVC0fI2a8GcHDoGkmA4QmxwBmMcDmL7ukXI"
+            }
+        } 
+
+        const data = await axios.get(`http://localhost:3001/user/profile`, axiosConfig)
+        
+        SetProfile(data.data.result)
+  
+      } catch(err) {
+        console.log(err.message)
+      }
+    }
+  
+    useEffect(() => {
+        getEventInfo();
+    }, [history])
+
     return (
         <MainContainer>
             <Container maxWidth="sm">
                 <CenterObjects>
                     <h1>Perfil</h1>
+                    <p>{profile.name}</p>
+                    <p>{profile.email}</p>
+                    <p>{profile.role}</p>
                     <h1>Seus ingressos</h1>
                 </CenterObjects>
             </Container>
